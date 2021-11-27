992dc8c104b4844c6e7259ac2983bbac3d34823e
tinoschroeter <mail@wgnetworks.de>
2021-11-27 23:12:20 +0100

    update: web

--- www/build.sh
+++ www/build.sh
@@ -2,4 +2,3 @@
 wget -O - https://raw.githubusercontent.com/tinoschroeter/k8s.homelab/master/docs/README.md 2>/dev/null \
 |pandoc --standalone --css style.css --metadata title="Homelab" \
 --include-before-body=ribbon.html -f markdown -t html > index.html
-# test dev branch
