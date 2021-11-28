8a5185491ad88d8c86d1743be322da2aad4893b0
tinoschroeter <mail@wgnetworks.de>
2021-11-29 00:07:33 +0100

    fix: uptime

--- button/routes/uptime.js
+++ button/routes/uptime.js
@@ -4,13 +4,13 @@ const { makeBadge, ValidationError } = require("badge-maker");
 
 const uptime = () => {
   return (req, res) => {
-    fs.readFile(temp, (err, data) => {
+    fs.readFile(temp, 'utf8', (err, data) => {
       if (!err) {
-        const result = Math.floor(data / 60 / 60 / 24);
+        const result = Math.floor(data.split(' ')[0] / 60 / 60 / 24);
         const color = result > 182 ? "red" : "brightgreen"
         const svg = makeBadge({
           label: "Uptime ", // (Optional) Badge label
-          message: ` ${result.toString()} C`, // (Required) Badge message
+          message: result.toString(), // (Required) Badge message
           labelColor: "#555", // (Optional) Label color
           color, // (Optional) Message color
           style: "flat", // (Optional) One of: 'plastic', 'flat', 
