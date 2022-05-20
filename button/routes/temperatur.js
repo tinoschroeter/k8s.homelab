c3bf5dedfbdd6f6df5c424506bdd1640e53b5a23
Tino Schroter <tino.schroeter@gmail.com>
2022-05-20 20:05:51 +0200

    add: more temperatur colors + update npm

--- button/routes/temperatur.js
+++ button/routes/temperatur.js
@@ -2,21 +2,31 @@ const fs = require("fs");
 const temp = "/sys/class/thermal/thermal_zone0/temp";
 const { makeBadge, ValidationError } = require("badge-maker");
 
+const getColor = (temperatur) => {
+  if (temperatur > 77) {
+    return "red";
+  } else if (temperatur > 42) {
+    return "yellow";
+  } else {
+    return "brightgreen";
+  }
+};
+
 const temperatur = () => {
   return (req, res) => {
     fs.readFile(temp, (err, data) => {
       if (!err) {
         const result = (data / 1000).toFixed(1);
-        const color = result > 77 ? "red" : "brightgreen"
+        const color = getColor(result);
         const svg = makeBadge({
           label: "CPU Temp ", // (Optional) Badge label
           message: ` ${result.toString()} C`, // (Required) Badge message
           labelColor: "#555", // (Optional) Label color
           color, // (Optional) Message color
-          style: "flat", // (Optional) One of: 'plastic', 'flat', 
-                         // 'flat-square', 'for-the-badge' or 'social'
+          style: "flat", // (Optional) One of: 'plastic', 'flat',
+          // 'flat-square', 'for-the-badge' or 'social'
         });
-        res.setHeader('Content-Type', 'image/svg+xml');
+        res.setHeader("Content-Type", "image/svg+xml");
         return res.send(svg);
       }
       res.json(err);
